eba615c88de9801e2b1320d175a1a29fef587b5c
iremkpr <38679907+iremkoprek@users.noreply.github.com>
2024-10-22 22:08:00 -0400

    configuration setup and alert- checkbox examples

---
+++ src/test/java/steps/test1.java
@@ -0,0 +1,13 @@
+package steps;
+
+import TestBase.BaseClass;
+
+public class test1 extends BaseClass{
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		setUp();
+		tearDown();
+	}
+
+}
