96f8086e2e237b343f8766f85df3958acd04416b
iremkpr <38679907+iremkoprek@users.noreply.github.com>
2024-10-23 09:30:39 -0400

    Amazon hw

--- src/test/java/steps/test2.java
+++ src/test/java/steps/test2.java
@@ -1,10 +1,9 @@
 package steps;
 
- 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import java.util.List;
-
+ 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,28 +14,30 @@ import utils.Constants;
 
 public class test2 {
 	public static void main(String[] args) throws InterruptedException {
+	/*// Go to https://amazon.com
+// select books from the search dropdown.
+// type Harry potter
+// and click on Search
+// check "Kindle Unlimited Eligible" checkbox on the left*/
 		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get(ConfigsReader.getProperty("url"));
+ 	
+		Thread.sleep(5000);
+		Select select=new Select(driver.findElement(By.xpath("//select[@id='searchDropdownBox']")));
+		select.selectByVisibleText("Books");
 		
-		WebElement female=driver.findElement(By.xpath("//label[@for='gender-radio-2']"));
-		female.click();
+		WebElement searchbox=driver.findElement(By.id("twotabsearchtextbox"));
+		searchbox.sendKeys("Harry potter");
+		
+		WebElement submit=driver.findElement(By.id("nav-search-submit-button"));
+		submit.click();
+		
 		Thread.sleep(1000);
-	
-		
-		List<WebElement> hobbies=driver.findElements(By.xpath("//div[@id='hobbiesWrapper']/div[2]//label"));
-		for(WebElement hobby:hobbies) {
-			if(hobby.isEnabled()) {
-				hobby.click();
-			}
-		}
-		Thread.sleep(3000);
-
-		
-		if(driver!=null) {
-			driver.quit();
-		}
-		
- 	}
+		WebElement checkbox=driver.findElement(By.xpath("//span[text()='Kindle Unlimited Eligible']/..//label"));
+		checkbox.click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
 }
